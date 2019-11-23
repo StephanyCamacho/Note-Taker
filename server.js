0ea6f411f8040158e9f7b9b1fa1a7af9c017ad16
Stephany Camacho-Lara <stephany_c19@yahoo.com>
2019-11-22 23:24:28 -0700

    port now connected

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-var express = require("express");
+// Dependencies
+const express = require("express");
+
+// Setting up Express App
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+//Routes
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
